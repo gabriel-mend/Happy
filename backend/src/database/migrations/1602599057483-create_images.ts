9d1ced8fa356b6d1bf93e929b7a198debb1191d2
Gabriel Mendonca <gabrielmendoncapereira14@gmail.com>
2020-10-13 13:56:16 -0300

    Backend criado!

---
+++ backend/src/database/migrations/1602599057483-create_images.ts
@@ -0,0 +1,43 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class createImages1602599057483 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: 'images',
+            columns: [
+                {
+                    name: 'id',
+                    type: 'integer',
+                    unsigned: true,
+                    isPrimary: true,
+                    isGenerated: true,
+                    generationStrategy: 'increment'
+                },
+                {
+                    name: 'path',
+                    type: 'varchar',
+                },
+                {
+                    name: 'orphanage_id',
+                    type: 'interger',
+                }
+            ],
+            foreignKeys: [
+                {
+                    name: 'imageOrphanage',
+                    columnNames: ['orphanage_id'],
+                    referencedTableName: 'orphanages',
+                    referencedColumnNames: ['id'],
+                    onUpdate: 'CASCADE',
+                    onDelete: 'CASCADE'
+                }
+            ]
+        }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('images');
+    }
+
+}
